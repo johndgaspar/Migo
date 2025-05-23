6f430752fcd4a81dd3ce73218e2fdc743b241469
Jonathan Gaspar <johndgaspar@gmail.com>
2025-05-22 21:35:53 -0500

    try again

---
+++ api/chat.js
@@ -0,0 +1,8 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  const { input } = req.body;
+  res.status(200).json({ reply: `You said: ${input}` });
+}
